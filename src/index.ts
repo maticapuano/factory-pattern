a04f702ba468f0856979b62590b410b895f27b2a
Matias <maticapuano97@gmail.com>
2020-08-16 12:40:22 -0300

    Implemented new changes superlib using factory pattern.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import { SuperLib } from "./lib/superLib";
 
 /**
  * Using: No using factory pattern
- */
-const lib = new SuperLib("Super Name", { next: true });
+ *
+ * const lib = new SuperLib("Super Name", { next: true }); //error in constructor resolved in factory pattern, not affect in constructor.
 
 console.log(lib.name);
 console.log(lib.options);
+*/
 
 /**
  * Using: implemented factory pattern
@@ -17,5 +18,6 @@ const libFactory = SuperLibFactory.create("Super factory", {
   next: true,
 });
 
+console.log(libFactory.id);
 console.log(libFactory.name);
 console.log(libFactory.options);
