098c2515a34845afebdcd032ce4f223f69fa6745
Matias <maticapuano97@gmail.com>
2020-08-16 12:52:06 -0300

    Added id option at options.

--- src/factories/superLib-factory.ts
+++ src/factories/superLib-factory.ts
@@ -1,8 +1,8 @@
 import { SuperLib } from "../lib/superLib";
 
 export class SuperLibFactory {
-  static create(name: string, options: {}) {
-    const id = Math.random().toString(32).slice(2);
+  static create(name: string, options: {}, id?: string) {
+    id = id ?? Math.random().toString(32).slice(2);
     return new SuperLib(id, name, options);
   }
 }
