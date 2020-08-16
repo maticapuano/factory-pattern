bd848df22863065a6b931ad34a92a6a331fc3b8a
Matias <maticapuano97@gmail.com>
2020-08-16 12:32:20 -0300

    Implemented factory pattern.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import { SuperLibFactory } from "./factories/superLib-factory";
 import { SuperLib } from "./lib/superLib";
 
+/**
+ * Using: No using factory pattern
+ */
 const lib = new SuperLib("Super Name", { next: true });
 
 console.log(lib.name);
 console.log(lib.options);
+
+/**
+ * Using: implemented factory pattern
+ */
+
+const libFactory = SuperLibFactory.create("Super factory", {
+  next: true,
+});
+
+console.log(libFactory.name);
+console.log(libFactory.options);
