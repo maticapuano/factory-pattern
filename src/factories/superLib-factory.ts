e811bae8bbc3a07a03946ec9504a18e3ef53e37c
Matias <maticapuano97@gmail.com>
2020-08-16 12:42:31 -0300

    Implemented id.

--- src/factories/superLib-factory.ts
+++ src/factories/superLib-factory.ts
@@ -2,6 +2,7 @@ import { SuperLib } from "../lib/superLib";
 
 export class SuperLibFactory {
   static create(name: string, options: {}) {
-    return new SuperLib(name, options);
+    const id = Math.random().toString(32).slice(2);
+    return new SuperLib(id, name, options);
   }
 }
